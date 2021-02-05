061a1ca64e9500417fba8fcad253c360b4b5292d
guimariz <guimariz@gmail.com>
2021-02-05 01:47:49 -0300

    Validando o uso do input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import React, { Component } from 'react';
+import InputFullSalary from './components/InputFullSalary';
 
-function App() {
-  return <div className="App">Teste</div>;
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fullSalary: 1000,
+    };
+  }
+
+  handleFullSalaryChange = (newValue) => {
+    this.setState({ fullSalary: newValue });
+  };
+
+  render() {
+    const { fullSalary } = this.state;
+    return (
+      <div className="container">
+        <h1>React Salario</h1>
+        <div className="row">
+          <InputFullSalary
+            currentValue={fullSalary}
+            onSalaryChange={this.handleFullSalaryChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
