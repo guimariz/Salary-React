f977d681a6f6e054f8c94acef6f0132699af6020
guimariz <guimariz@gmail.com>
2021-02-05 02:14:52 -0300

    Projeto finalizado

--- src/components/InputReadOnly.js
+++ src/components/InputReadOnly.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
+import { formatPercentage, formatMoney } from '../helpers/formatters';
 
 export default class InputReadOnly extends Component {
   render() {
     const { color = 'black', value, percentage = 0, label } = this.props;
 
-    const formattedPercentage = percentage > 0 ? `(${percentage})` : '';
-    const formattedValue = `${value} ${formattedPercentage}`;
+    const formattedPercentage =
+      percentage > 0 ? `(${formatPercentage(percentage)})` : '';
+    const formattedValue = `${formatMoney(value)} ${formattedPercentage}`;
     return (
       <div>
-        <input
-          className="input-field col s12 m6 l3"
-          autoFocus
-          id="inputReadOnly"
-          value={formattedValue}
-          readOnly
-        />
-        <label className="active" htmlFor="inputFullSalary">
-          {label}
-        </label>
+        <input autoFocus id="inputReadOnly" value={formattedValue} readOnly />
+        <label htmlFor="inputFullSalary">{label}</label>
       </div>
     );
   }
